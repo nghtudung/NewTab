e12b5d5202ad58fb8394e31f8fbd98dac090876d
nghtudung <nghtudung@gmail.com>
2025-03-20 01:16:59 +0700

    update time show

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ function updateDateTime() {
     const dateString = `${dayOfWeek}, ${month} ${date}`;
 
     // Format the time: H:MM AM/PM
-    let hours = now.getHours();
+    let hours = now.getHours().toString().padStart(2, "0");
     const minutes = now.getMinutes().toString().padStart(2, "0");
     // const seconds = now.getSeconds().toString().padStart(2, "0");
     // const ampm = hours >= 12 ? "PM" : "AM";
